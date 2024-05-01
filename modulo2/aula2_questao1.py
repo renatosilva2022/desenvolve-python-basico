018d1bb8891c1cca8506c57c9285426faca23489
renatosilva2022 <107969416+renatosilva2022@users.noreply.github.com>
2024-05-01 20:53:06 -0300

    Update aula2_questao1.py

--- modulo2/aula2_questao1.py
+++ modulo2/aula2_questao1.py
@@ -1,1 +1,12 @@
+# Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+# Armazene o valor 5 em uma variável e o valor 2 em outra variável
+# Imprima o tipo de dados dessas duas variáveis
+# Divida a primeira variável (com o valor 5) pela segunda variável (com o valor 2) e armazene o resultado em uma terceira variável
+# Imprima a terceira variável e também o seu tipo
 
+# Algoritmo
+# Leia e armazene o número n que deseja multiplicar por 9#Entrada
+# Subtraia n-1 e armazenar como o primeiro díigito n1 #Processamento
+# Subtraia 10-n e armazenar como o segundo dígito n2 #Processamento
+# Efetue a expressão (n1-10) + n2 e armazene como resultado final e 
+# Mostre na tela n1 e n2 e encerre o programa #Saída
